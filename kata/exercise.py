8b98714616609a7bc4d10c479fb79bb2b18517c6
Todd Benson <todd.benson@csaa.com>
2017-01-18 17:34:40 -0700

    Today's work

--- kata/exercise.py
+++ kata/exercise.py
@@ -20,66 +20,72 @@ print htmlize(cells)
 
 
 def get_generation(cells, generation):
+    def cell_exists(r, c):
+        return True if 0 <= r < len(cells) and 0 <= c < len(cells[r]) else False
+
+    def get_active_neighbors(r, c):
+        n = 0
+        if cell_exists(r - 1, c - 1):
+            n += cells[r - 1][c - 1]
+        if cell_exists(r - 1, c):
+            n += cells[r - 1][c]
+        if cell_exists(r - 1, c + 1):
+            n += cells[r - 1][c + 1]
+        if cell_exists(r, c - 1):
+            n += cells[r][c - 1]
+        if cell_exists(r, c + 1):
+            n += cells[r][c + 1]
+        if cell_exists(r + 1, c - 1):
+            n += cells[r + 1][c - 1]
+        if cell_exists(r + 1, c):
+            n += cells[r + 1][c]
+        if cell_exists(r + 1, c + 1):
+            n += cells[r + 1][c + 1]
+        return n
+
+    def set_value(neighbors, current):
+        if neighbors == 3:
+            return 1
+        if neighbors == 2:
+            return current
+        return 0
+
+    def add_bottom_row(add_bottom_row_array):
+        add_bottom_row_array.append(
+            [set_value(get_active_neighbors((len(add_bottom_row_array) + 1), cell), 0) for cell in
+             range(len(add_bottom_row_array[0]))])
+        return add_bottom_row_array
+
+    def clean_up(clean_up_array):
+        if sum(clean_up_array[0]) == 0:
+            clean_up_array = clean_up_array[1:]
+        if sum(clean_up_array[-1]) == 0:
+            clean_up_array = clean_up_array[:-1]
+        return clean_up_array
+
+    def add_columns(add_column_array):
+        n = 0
+        for x in add_column_array:
+            if x[-1] > 0:
+                n += 1
+                if n >= 3:
+                    add_column_array = [x + [0] for x in add_column_array]
+        for x in add_column_array:
+            if x[-1] > 0:
+                n += 1
+                if n >= 3:
+                    add_column_array = [x[1:] for x in add_column_array]
+        return add_column_array
+
+    def generate(generate_array):
+        generate_array = add_bottom_row(generate_array)
+
+        # OFFENDING CODE!
+        generate_array = [[set_value(get_active_neighbors(row, cell), generate_array[row][cell])
+                           for cell in range(len(generate_array[row]))
+                           if cell_exists(row, cell)]
+                          for row in range(len(generate_array))]
+        return clean_up(generate_array)
+    cells = add_columns(cells)
     print(cells)
-
-    lgth = len(cells[0]) + 2
-    arr = [0 for x in range(lgth)]
-    for y in range(len(cells)):
-        arr.extend([0])
-        arr.extend([cells[y][x] for x in range(lgth - 2)])
-        arr.extend([0])
-    arr.extend([0 for x in range(len(cells[0]) + 2)])
-
-    def check_neighbors(arr):
-        answer = list([])
-        for y in range(lgth + 1, len(arr)):
-            neighbor = 0
-            if arr[y - lgth + 1] == 1:
-                neighbor += 1
-            if arr[y - lgth] == 1:
-                neighbor += 1
-            if arr[y - lgth - 1 ] == 1:
-                neighbor += 1
-            if arr[y - 1] == 1:
-                neighbor += 1
-            if y < len(arr) - 1:
-                if arr[y + 1] == 1:
-                    neighbor += 1
-            if y < len(arr) - lgth:
-                if arr[y + lgth] == 1:
-                    neighbor += 1
-            if y < len(arr) - 1 - lgth:
-                if arr[y + 1 + lgth] == 1:
-                    neighbor += 1
-            if y < len(arr) + 1 - lgth:
-                if arr[y - 1 + lgth] == 1:
-                    neighbor += 1
-            answer.extend([neighbor])
-        return answer
-
-    def new_state(arr):
-        answer = list([])
-        for y in range(len(arr)):
-            if arr[y] < 2 or arr[y] > 3:
-                answer.extend([0])
-            if arr[y] == 3:
-                answer.extend([1])
-            if arr[y] == 2:
-                answer.extend([2])
-        return answer
-
-    test = check_neighbors(arr)
-    print(test)
-
-    test2 = new_state(test)
-    print(test2)
-
-    new_test = [test2[x:x + lgth] for x in range(0, len(test2), lgth)]
-
-    print(new_test)
-
-    # another_test = [new_test[list] for list in new_test if new_test[list] == 2 else new_test[list] = cells[list]]
-    #
-    # print(another_test)
-
-    return cells
+    return cells if generation < 1 else get_generation(generate(cells), generation - 1)
--- tests/exercise_test.py
+++ tests/exercise_test.py
@@ -13,3 +13,35 @@ def test1():
     assert get_generation(start, 1) == end
 
 
+def test2():
+    start = [[1,0,0],
+             [0,1,1],
+             [1,1,0]]
+    end = [[1,0,1],
+           [0,1,1],
+           [0,1,0]]
+    assert get_generation(start, 2) == end
+
+
+def test3():
+    start = [[1,0,0],
+             [0,1,1],
+             [1,1,0]]
+    end = [[0,0,1],
+           [1,0,1],
+           [0,1,1]]
+    assert get_generation(start, 3) == end
+
+
+def test4():
+    start = [[1,0,0],
+             [0,1,1],
+             [1,1,0]]
+    end = [[1,0,0],
+           [0,1,1],
+           [1,1,0],
+           [0,0,0],
+           [0,0,0],
+           [0,0,0],
+           [0,0,0]]
+    assert get_generation(start, 40) == end
