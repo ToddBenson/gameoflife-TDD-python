cf412eff10c7f086b3d3fcd3b8e380dc9804b6ae
Todd Benson <todd.benson@csaa.com>
2017-01-20 01:50:18 -0700

    Refactored and made some changes

--- kata/exercise.py
+++ kata/exercise.py
@@ -1,38 +1,49 @@
+def cell_exists(r, c, array):
+    return True if 0 <= r < len(array) and 0 <= c < len(array[r]) else False
+
+
+def get_number_of_active_neighbors(row, column, array):
+    neighbors = [[-1, -1], [-1, 0], [-1, +1], [0, -1], [0, +1], [+1, -1], [+1, 0], [+1, +1]]
+    return sum([array[row + x[0]][column + x[1]] for x in neighbors if
+                cell_exists(row + x[0], column + x[1], array)])
+
+
+def set_cell_status(alive_neighbors, current_value):
+    return 1 if alive_neighbors == 3 else current_value if alive_neighbors == 2 else 0
+
+
+def print_generation(array):
+    for row in array:
+        print(row)
+
+
+def remove_empty_boarders(array):
+    while not any(array[0]): del array[0]
+    while not any(array[-1]): del array[-1]
+    while not any([row[0] for row in array]): map(lambda x: x.pop(0), array)
+    while not any([row[-1] for row in array]): map(lambda x: x.pop(), array)
+    return array
+
+
+def add_empty_boarders(array):
+    array = [[0] + column + [0] for column in array]
+    array.append([0 for column in range(len(array[0]))])
+    array.insert(0, [0 for column in range(len(array[0]))])
+    return array
+
+
+def create_next_generation(array):
+    array = [[set_cell_status(get_number_of_active_neighbors(row, column, array), array[row][column])
+              for column in range(len(array[row]))
+              if cell_exists(row, column, array)]
+             for row in range(len(array))]
+    return array
+
+
 def get_generation(current_generation, generation):
+    current_generation = add_empty_boarders(current_generation)
+    current_generation = create_next_generation(current_generation)
+    current_generation = remove_empty_boarders(current_generation)
+    # print_generation(current_generation)
 
-    for counter in range(0, generation):
-
-        def cell_exists(r, c):
-            return True if 0 <= r < len(current_generation) and 0 <= c < len(current_generation[r]) else False
-
-        def get_active_neighbors(row, column):
-            neighbors = [[-1, -1], [-1, 0], [-1, +1], [0, -1], [0, +1], [+1, -1], [+1, 0], [+1, +1]]
-            return sum([current_generation[row + x[0]][column + x[1]] for x in neighbors if cell_exists(row + x[0], column + x[1])])
-
-        def set_alive_or_dead(alive_neighbors, current_value):
-            return 1 if alive_neighbors == 3 else current_value if alive_neighbors == 2 else 0
-
-        def clean_up(array):
-            while not any(array[0]): del array[0]
-            while not any(array[-1]): del array[-1]
-            while not any([row[0] for row in array]): map(lambda x: x.pop(0), array)
-            while not any([row[-1] for row in array]): map(lambda x: x.pop(), array)
-            return array
-
-        def expand(array):
-            array = [[0] + column + [0] for column in array]
-            array.append([0 for column in range(len(array[0]))])
-            array.insert(0, [0 for column in range(len(array[0]))])
-            return array
-
-        def create_next_generation(array):
-            array = [[set_alive_or_dead(get_active_neighbors(row, column), array[row][column])
-                      for column in range(len(array[row]))
-                      if cell_exists(row, column)]
-                     for row in range(len(array))]
-            return clean_up(array)
-
-        current_generation = expand(current_generation)
-        current_generation = create_next_generation(current_generation)
-
-    return current_generation
+    return current_generation if generation <= 1 else get_generation(current_generation, generation - 1)
--- tests/exercise_test.py
+++ tests/exercise_test.py
@@ -1,6 +1,11 @@
 
 """test the kata task"""
 from exercise import get_generation
+from exercise import add_empty_boarders
+from exercise import remove_empty_boarders
+from exercise import set_cell_status
+from exercise import get_number_of_active_neighbors
+from exercise import cell_exists
 
 
 def test1():
@@ -59,3 +64,103 @@ def test5():
     assert get_generation(start, 10) == end
 
 
+def test6():
+    start = [[1],
+             [1],
+             [1]]
+    end = [[1, 1, 1]]
+    assert get_generation(start, 5) == end
+
+
+def test7():
+    start = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    end = [[1, 1, 1]]
+    assert get_generation(start, 1) == end
+
+
+def test8():
+    start = [[1,1,1,1]]
+    end = [[0,1,1,0],
+           [1,0,0,1],
+           [0,1,1,0]]
+    assert get_generation(start, 40) == end
+
+
+def test9():
+    start = [[1,1,1],
+             [1,1,1],
+             [1,1,1]]
+    end = [[0,0,0,0,0],
+           [0,1,1,1,0],
+           [0,1,1,1,0],
+           [0,1,1,1,0],
+           [0,0,0,0,0]]
+    assert add_empty_boarders(start) == end
+
+
+def test10():
+    end = [[1,1,1],
+             [1,1,1],
+             [1,1,1]]
+    start = [[0,0,0,0,0],
+           [0,1,1,1,0],
+           [0,1,1,1,0],
+           [0,1,1,1,0],
+           [0,0,0,0,0]]
+    assert remove_empty_boarders(start) == end
+
+
+def test11():
+    assert set_cell_status(1,1) == 0
+
+
+def test12():
+    assert set_cell_status(2,1) == 1
+
+
+def test13():
+    assert set_cell_status(3,0) == 1
+
+
+def test14():
+    start = [[0,0,0,0,0],
+             [0,1,1,1,0],
+             [0,1,1,1,0],
+             [0,1,1,1,0],
+             [0,0,0,0,0]]
+    assert get_number_of_active_neighbors(1,1,start) == 3
+
+
+def test15():
+    start = [[0,0,0,0,0],
+             [0,1,1,1,0],
+             [0,1,1,1,0],
+             [0,1,1,1,0],
+             [0,0,0,0,0]]
+    assert get_number_of_active_neighbors(4,4,start) == 1
+
+
+def test16():
+    start = [[0,0,0,0,0],
+             [0,1,1,1,0],
+             [0,1,1,1,0],
+             [0,1,1,1,0],
+             [0,0,0,0,0]]
+    assert cell_exists(5,5,start) is False
+
+
+def test17():
+    start = [[0,0,0,0,0],
+             [0,1,1,1,0],
+             [0,1,1,1,0],
+             [0,1,1,1,0],
+             [0,0,0,0,0]]
+    assert cell_exists(4,4,start) is True
