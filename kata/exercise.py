bb739e078f68a046d70f7dbb107ad68d244fde51
Todd Benson <todd.benson@csaa.com>
2017-01-20 02:11:56 -0700

    Refactored and made some changes

--- kata/exercise.py
+++ kata/exercise.py
@@ -1,20 +1,15 @@
-def cell_exists(r, c, array):
-    return True if 0 <= r < len(array) and 0 <= c < len(array[r]) else False
+def cell_exists(row, column, array):
+    return True if 0 <= row < len(array) and 0 <= column < len(array[row]) else False
 
 
 def get_number_of_active_neighbors(row, column, array):
     neighbors = [[-1, -1], [-1, 0], [-1, +1], [0, -1], [0, +1], [+1, -1], [+1, 0], [+1, +1]]
-    return sum([array[row + x[0]][column + x[1]] for x in neighbors if
-                cell_exists(row + x[0], column + x[1], array)])
+    return sum([array[row + offset[0]][column + offset[1]] for offset in neighbors if
+                cell_exists(row + offset[0], column + offset[1], array)])
 
 
 def set_cell_status(alive_neighbors, current_value):
     return 1 if alive_neighbors == 3 else current_value if alive_neighbors == 2 else 0
-
-
-def print_generation(array):
-    for row in array:
-        print(row)
 
 
 def remove_empty_boarders(array):
@@ -26,24 +21,20 @@ def remove_empty_boarders(array):
 
 
 def add_empty_boarders(array):
-    array = [[0] + column + [0] for column in array]
-    array.append([0 for column in range(len(array[0]))])
-    array.insert(0, [0 for column in range(len(array[0]))])
-    return array
+    expanded_array = [[0] + column + [0] for column in array]
+    expanded_array.append([0 for column in range(len(expanded_array[0]))])
+    expanded_array.insert(0, [0 for column in range(len(expanded_array[0]))])
+    return expanded_array
 
 
 def create_next_generation(array):
-    array = [[set_cell_status(get_number_of_active_neighbors(row, column, array), array[row][column])
-              for column in range(len(array[row]))
-              if cell_exists(row, column, array)]
-             for row in range(len(array))]
-    return array
+    next_generation = [[set_cell_status(get_number_of_active_neighbors(row, column, array), array[row][column])
+                       for column in range(len(array[row]))
+                       if cell_exists(row, column, array)]
+                       for row in range(len(array))]
+    return next_generation
 
 
-def get_generation(current_generation, generation):
-    current_generation = add_empty_boarders(current_generation)
-    current_generation = create_next_generation(current_generation)
-    current_generation = remove_empty_boarders(current_generation)
-    # print_generation(current_generation)
-
+def get_generation(cells, generation):
+    current_generation = remove_empty_boarders(create_next_generation(add_empty_boarders(cells)))
     return current_generation if generation <= 1 else get_generation(current_generation, generation - 1)
